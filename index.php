2d62bfea06439fe7ec7363de369cb784b469a118
Salo Valdez <salovaldez.net@gmail.com>
2022-04-13 15:52:47 -0500

    dasdasd
    dasdsad

--- index.php
+++ index.php
@@ -1,1 +1,245 @@
-<? echo "hola mundillooooo"; ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+ 
+
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Marcar Asistencia:::ENEI</title>
+<link href="admin/views/src/img/inei/favicon.ico" rel="shortcut icon" type="image/x-icon">
+  <link rel="stylesheet" href="./admin/views/src/librerias/sweetAlert/sweet-alert.css">
+
+<!-- MIS ESTILOS -->
+  <link rel="stylesheet" href="./admin/views/src/css/ui-asistencia.css">
+
+  <link rel="stylesheet" href="./admin/views/src/fuentes/icomoon/icomoon_v1/icomoon_v1.css">
+  <link rel="stylesheet" href="./admin/views/src/fuentes/icomoon/icomoon.css">
+
+</head>
+<body>
+        <div class="contenedor">
+              <div class="cabecera">
+                       <div class="cab cabeceraIzquierdo">
+                            <a href="#"> <img src="./admin/views/src/img/inei/logoEnei.png" alt=""></a>
+                       </div>
+
+                       <ul class="cab cabeceraDerecho">
+                            <li ><a href="#" class="men"><i style=""class="icono icon icon-cog"> </i> Panel de Administracion <i class="icono derecha icon-chevron-down"></i></a>
+                               <ul  class="subMenu">
+
+                                   <span><a class="contAvatar" href="#"> <i class="icon icon-user-tie"></i> </a> </span>
+                                   <hr>
+                                   <form id="formUsuario">
+                                         <div class="contenedorInput">
+                                           <span><i class="icono icon icon-user"></i><input  name="usuario" id="usuario" value="" type="text" placeholder="Usuario"></span>
+                                         </div>
+                                         <div class="contenedorInput">
+                                             <span><i class="icono icon-lock-open"></i><input name="contrasena" id="contrasena" value="" type="text"  placeholder="Contraseña"></span>
+                                         </div>
+                                         <div class="contenedorInput">
+
+                                          <input type="button" name="" value="Entrar" id="btnEntrarAdmin" >
+                                         </div>
+                                   </form>
+                               </ul>
+                             </li>
+                       </ul>
+              </div>
+
+              <div class="contenedorActivos">
+                  <div class="contenedorFormularioAsistencia">
+                    <div class="contenedorReloj">
+                            <!-- <input type="text" name="" value="" placeholder="12:12:12"> -->
+                            <div class="hora" id="time">12:12:12</div>
+                    </div>
+                    <div class="formulario">
+                      <form id="formularioRegistro">
+                            <div class="contenedorInput">
+
+                              <span><i class="icono icon icon-user"></i>
+                              <input type="text" name="usuarioRegistro" id="usuarioRegistro" value=""  placeholder="usuario"></span>
+                            </div>
+                            <div class="contenedorInput">
+                                <span><i class="icono icon-lock-open"></i>
+                                <input type="text" name="contrasenaRegistro" id="contrasenaRegistro" value="" placeholder="contraseña"></span>
+                            </div>
+                            <div class="contenedorInput">
+                              <input id="btnRegistro"  type="button" name="" value="Marcar">
+                            </div>
+                      </form>
+                    </div>
+
+                    <!--TODO:  CERRAR FORMULARIO BOTON  -->
+<!-- <div class="contenedor_boton">
+  <span class="cerrarFormulario"  id="cerrarFormularioFirma">
+      <span class="icono">x</span>
+  </span>
+</div> -->
+
+
+
+
+<div class="contenedor_boton_float_cerrar_formulario">
+  <div class="boton_cerrar_formulario">
+    <i class="icon icon-cross icon-cross"></i>
+  </div>
+</div>
+
+                  </div>
+                  <div class="contenedorUsuariosActivos">
+
+
+
+<div class="tabs">
+  <div class="tabs-header">
+    <div class="border"></div>
+    <ul>
+      <li class="active"><a href="#tab-1" tab-id="1" ripple="ripple" ripple-color="#FFF">Practicantes</a></li>
+      <li><a href="#tab-2" tab-id="2" ripple="ripple" ripple-color="#FFF">Docentes</a></li>
+    </ul>
+    <nav class="tabs-nav"><i class="material-icons" id="prev" ripple="ripple" ripple-color="#FFF" ><i class="icono derecha   icon-triangle-left
+
+"></i></i><i class="material-icons" id="next" ripple="ripple" ripple-color="#FFF"><i class="icono derecha  icon-triangle-right
+
+"></i></i></nav>
+  </div>
+  <div class="tabs-content">
+    <div class="tab active" tab-id="1"> 
+        <div class="activos" id="emleados_practicante_presente"> </div>
+    </div>
+    <div class="tab" tab-id="2">
+
+    
+    <div class="activos" id="docentes_practicante_presente"> </div>
+      <!-- 2. Donec ullamcorper nulla non metus auctor fringilla. Aenean eu leo quam. -->
+     
+  </div>
+</div>
+<!-- <div class="pen-footer"><a href="https://www.behance.net/gallery/31103291/Tabs-UI-Library" target="_blank"><i class="material-icons">arrow_backward</i>View on Behance</a><a href="https://github.com/andyhqtran/UI-Library/tree/master/Tabs" target="_blank">View on Github<i class="material-icons">arrow_forward</i></a></div> -->
+
+
+
+
+
+
+
+
+
+
+<form action="">
+<input type="text" name="nombres" id="nombres"><br>
+<input type="text" name="apellidos" id="apellidos"><br>
+<input type="text" name="apesss" value="sortess">
+</form>
+
+<button id="verificar">verificar</button>
+<br><br>
+
+
+
+
+                              <!-- <div class="activos" id="emleados_practicante_presente">
+                                  
+                              </div> -->
+
+                    <div class="status">
+                      <div class="izquierda"></div>
+                      <div class="derecha">
+
+
+                        <span class="barFooter" style="color:green;"><div class="circle-barFotter" style="background-color:green;" id="barFooterTotalAsistentes">  0</div> Total Asistentes</span>
+                         <span class="barFooter" style="color:blue;"><div class="circle-barFotter" style="background-color:blue;" id="barFooterTotalDocentes">   0</div> Docentes</span>
+                         <span class="barFooter" style="color:red; "><div class="circle-barFotter" style="background-color:red;"id="barFooterTotalPracticantes">  0 </div> Practicantes</span>
+                       </div>
+                    </div>
+                  </div>
+
+
+
+                  <div class="contenedor_boton_float">
+                    <div class="fab fab--normal">
+                      <i class="icon icon-list icon-list"></i>
+                    </div>
+                  </div>
+<!-- icon-list -->
+
+
+
+              </div>
+        </div>
+      <!--  MIS SCRIPT -->
+      <script type="text/javascript" src="./admin/views/src/librerias/jquery/jquery-3.3.1.min.js"></script>
+      <script src="./admin/views/src/librerias/sweetAlert/sweet-alert.min.js"></script>
+      <script type="text/javascript" src="./admin/views/src/js/ui-asistencia.js"></script>
+
+      
+      <script type="text/javascript"  src="./views/ajax/sesion_admin.js"></script>
+      <script type="text/javascript"  src="./views/ajax/registro_asistencia.js"></script>
+
+      <script type="text/javascript"  src="./views/ajax/listarNumAsistentes.js"></script>
+
+
+
+
+      <script type="text/javascript"  src="views/src/js/reloj.js"></script>
+      <script type="text/javascript"  src="views/src/js/tab_Inicio.js"></script>
+
+
+      <script type="text/javascript">
+                $(document).ready(function(){
+                  cerrarPanelSesiones();
+                  abrirPanelSesiones();
+                });
+
+                function  cerrarPanelSesiones(){
+                    $(".boton_cerrar_formulario").click(function(){
+                          $(".contenedorFormularioAsistencia").slideUp();
+                $(".fab--normal").slideDown();
+
+                    });
+                }
+                function abrirPanelSesiones(){
+                    $(".fab--normal").click(function(){
+                            $(".contenedorFormularioAsistencia").slideDown();
+                    $(".fab--normal").slideUp();
+
+                    });
+                }
+
+                const container = document.querySelector('.container');
+                const mainFab = document.querySelector('.fab--normal');
+                // const smallFabs = document.querySelectorAll('.fab--small');
+                const add = document.querySelector('.add');
+                const edit = document.querySelector('.edit');
+
+                function openFab() {
+                  mainFab.classList.add('grow');
+                  add.classList.add('rotate-out');
+                  edit.classList.add('rotate-in');
+                  // smallFabs.forEach(fab => fab.classList.add('expand'))
+                }
+
+                function closeFab() {
+                  mainFab.classList.remove('grow');
+                  add.classList.remove('rotate-out');
+                  edit.classList.remove('rotate-in');
+                  // smallFabs.forEach(fab => fab.classList.remove('expand'));
+                }
+
+                mainFab.addEventListener('mouseenter', openFab);
+                container.addEventListener('mouseleave', closeFab);
+
+
+
+
+
+          </script>
+
+
+
+
+</body>
+</html>
